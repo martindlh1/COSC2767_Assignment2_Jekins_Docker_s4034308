4f43a6857d1e4e23efe3b30fa9eb2d198f14d61a
martindlh1 <72010190+martindlh1@users.noreply.github.com>
2023-12-22 10:10:24 +0100

    Update configure_docker_container.sh

--- configure_docker_container.sh
+++ configure_docker_container.sh
@@ -1,9 +1,18 @@
-#remove all container image and container
-docker rm -vf $(docker ps -aq)
-docker rmi -f $(docker images -aq)
+#create image if not existing
+docker image inspect tomcat-image
+if [ $? -eq 1 ]
+then
+    rm Dockerfile
+    echo "FROM tomcat:latest" >> Dockerfile
+    echo "RUN cp -R /usr/local/tomcat/webapps.dist/* /usr/local/tomcat/webapps" >> Dockerfile
+    echo "COPY ./*.war /usr/local/tomcat/webapps" >> Dockerfile
+    docker build -t tomcat-image .
+fi
+  
 
-#create a docker image from Dockerfile
-docker build -t tomcat-image .
+#remove container if existing
+docker stop tomcat-container
+docker rm tomcat-container
 
 #create a container from the previous image created
 docker run -d --name tomcat-container -p 8081:8080 tomcat-image

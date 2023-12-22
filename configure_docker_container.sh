b4a0ae4ec3ca38989a1d762e249b831955c01ce1
root <root@ip-172-31-84-188.ec2.internal>
2023-12-22 06:41:56 +0000

    [ADD] script & dockerfile

---
+++ configure_docker_container.sh
@@ -0,0 +1,5 @@
+#create a docker image from Dockerfile
+sudo docker build -t tomcat-image .
+
+#create a container from the previous image created
+docker run -d --name tomcat-container -p 8081:8080 tomcat-image
---
+++ configure_tomcat_server.sh
@@ -0,0 +1,16 @@
+sudo yum install -y docker
+
+#start docker
+service docker start
+
+#configure admindocker for jenkins
+sudo useradd dockeradmin
+sudo passwd dockeradmin << EOD
+devops-jenkins
+devops-jenkins
+EOD
+sudo usermod -aG docker dockeradmin
+
+#authorize password authentication
+sudo sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/' /etc/ssh/sshd_config
+sudo service sshd reload

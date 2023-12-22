67e1621c8f6ee0ddb312a2b89d7deb94d695ddf2
root <root@ip-172-31-84-188.ec2.internal>
2023-12-22 08:27:10 +0000

    [MOD] add remove container&images in container script

--- configure_docker_container.sh
+++ configure_docker_container.sh
@@ -1,3 +1,7 @@
+#remove all container image and container
+docker rm -vf $(docker ps -aq)
+docker rmi -f $(docker images -aq)
+
 #create a docker image from Dockerfile
 docker build -t tomcat-image .
 

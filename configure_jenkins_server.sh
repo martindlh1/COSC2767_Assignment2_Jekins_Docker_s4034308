1b63069216fbb73ef35b2182c382f8a11da33e15
martindlh1 <72010190+martindlh1@users.noreply.github.com>
2023-12-24 08:39:25 +0100

    Update configure_jenkins_server.sh

--- configure_jenkins_server.sh
+++ configure_jenkins_server.sh
@@ -1,13 +1,10 @@
 #install java
-sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
-sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
 sudo amazon-linux-extras install -y java-openjdk11
 
-#install jenkins
+#install jenkins and start jenkins
+sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
+sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
 sudo yum install -y jenkins
-
-#install maven
-yum install -y maven
 
 #display initila jenkins password to connect to dashboard
 echo -n "Initial Jenkins Password: "
